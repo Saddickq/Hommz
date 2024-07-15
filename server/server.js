4fea4ebf7fa5c7f5bc768004f329fe3ad8c150b5
Abubakr Saddiq <abubakrsaddickq@gmail.com>
2024-07-15 01:32:20 +0000

    include frontend url

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: ["https://hommz.netlify.app", "http://localhost:5173"]
+    origin: ["https://hommz.netlify.app"]
   })
 );
 
